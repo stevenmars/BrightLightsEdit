07ee984b27c9f3fed7f37866589cc5f8522b1d64
Kerr Macpherson <kerrmacpherson.work@gmail.com>
2018-02-20 22:46:47 +0000

    Endless Runner is now Endless
    Added ground generation with gaps to jump across. This feature will be replaced with constant ground and jumpable obstacles in the future.

---
+++ BrightLights Runner/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour {
+
+    public GameObject theGround;
+    public Transform generationPoint;
+
+    private float groundWidth; //also not needed maybe
+
+	// Use this for initialization
+	void Start () {
+        groundWidth = theGround.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + groundWidth+15, transform.position.y, transform.position.z);
+            //generate new ground
+            Instantiate(theGround, transform.position, transform.rotation);
+        }
+	}
+}
