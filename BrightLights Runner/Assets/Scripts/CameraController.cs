d9317955b7447ce6bcc9baa6b96c68de9f7180a5
Kerr Macpherson <kerrmacpherson.work@gmail.com>
2018-03-06 17:20:56 +0000

    Fade Background Brightness
    Added functionality to the camera so that the background colour changes from white to black as you play

--- BrightLights Runner/Assets/Scripts/CameraController.cs
+++ BrightLights Runner/Assets/Scripts/CameraController.cs
@@ -8,10 +8,13 @@ public class CameraController : MonoBehaviour {
 
     private Vector3 lastSquarePosition;
     private float distanceToMove;
+    Camera cam;
 
 	void Awake() {
         squarePlayer = FindObjectOfType<PlayerController>();
         lastSquarePosition = squarePlayer.transform.position;
+        cam = GetComponent<Camera>();
+        cam.clearFlags = CameraClearFlags.SolidColor;
     }
 	
     //camera tracks player movement on x axis
@@ -19,5 +22,8 @@ public class CameraController : MonoBehaviour {
         distanceToMove = squarePlayer.transform.position.x - lastSquarePosition.x;
         transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
         lastSquarePosition = squarePlayer.transform.position;
-	}
+        //fade the background colour from white to black
+        float t = Mathf.Lerp(Time.time, 3, 0)/30;
+        cam.backgroundColor = Color.Lerp(Color.white, Color.black, t);
+    }
 }
