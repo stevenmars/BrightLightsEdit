b870e56c2fe8c2d71104e04f9e11ca87d827c081
Kerr Macpherson <kerrmacpherson.work@gmail.com>
2018-04-08 15:03:22 +0100

    Added Main Menu
    You dont get dropped into the game straight away anymore

---
+++ BrightLights Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string sceneName;
+
+    public void PlayGame()
+    {
+        EditorSceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
